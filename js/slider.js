085bfbcb9a099b868c65efbc0f55e1904ddc5ba5
yourlorents <irynaslobodianykk@gmail.com>
2026-02-27 21:00:22 +0100

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,16 @@
 const swiper = new Swiper('.swiper-container', {
   loop: true,
   slidesPerView: 1,
+  centeredSlides: true,
   spaceBetween: 16,
   breakpoints: {
     768: {
       slidesPerView: 2,
+      centeredSlides: false,
     },
     1280: {
       slidesPerView: 3,
+      centeredSlides: false,
     },
   },
   pagination: {
